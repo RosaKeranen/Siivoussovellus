d9974be6e38be05e622ffcbf97c9d6a1b30e6f4d
Rosa Keränen <s3kero01@students.osao.fi>
2023-12-08 10:41:37 +0200

    Aloitettu tiedostoon tallentamisen tekeminen.

---
+++ SiivoApp/SiivoApp/DTO/ItemListRow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiivoApp.DTO
+{
+    [Serializable]
+    internal class ItemListRow
+    {
+        public string Text { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public ItemListRow() { }
+    }
+}
---
+++ SiivoApp/SiivoApp/Helpers/FileHelper.cs
@@ -0,0 +1,29 @@
+﻿using SiivoApp.DTO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Newtonsoft.Json
+
+namespace SiivoApp.Helpers
+{
+    internal class FileHelper
+    {
+        public FileHelper() { }
+
+        public void WriteToFile(List<ItemListRow> itemListRows) 
+        {
+            using (var stream = new FileStream("jipJap.json", FileMode.Create))
+            {
+                //var serializer = new DataContractJsonSerializer(typeof(List<InputItem>));
+                //serializer.WriteObject(stream, data);
+            }
+        }
+
+        //public List<ItemListRow> ReadFromFile()
+        //{
+        //}
+    }
+}
